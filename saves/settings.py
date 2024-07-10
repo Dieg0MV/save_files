596ce47e71774f37fddabee160cb2f4431b40638
Dieg0MV <diegoma.v2004@gmail.com>
2024-07-10 14:19:00 -0600

    Cambios de almacenamiento

--- saves/settings.py
+++ saves/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(" direccion del base_dir", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -122,7 +121,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
--- web/views.py
+++ web/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .forms import document
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
   form = document()
   if request.method == 'POST':
-    form = document(request.POST, request.FILES)
+    print(request.FILES)
+    form = document(request.POST, request.FILES) 
     if form.is_valid():
       form.save()
-  
+      return HttpResponse('sucess')
+    else:
+      
+      return HttpResponse('error')
   return render(request, 'index.html', {'form':form})
